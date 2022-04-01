247b7007968096f83e4fba45e5368532f5d4b1f3
0xmaharishi <cm972646@gmail.com>
2022-04-01 11:17:36 +0100

    feat: rewards distribution

--- contracts/AuraPenaltyForwarder.sol
+++ contracts/AuraPenaltyForwarder.sol
@@ -1,9 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-// Simple:
-//  - recieves AURA
-//  - forwards to most recent epoch after 4 weeks have elapsed & every 4 weeks following
-contract AuraPenaltyForwarder {
+import { IExtraRewardsDistributor } from "./Interfaces.sol";
+import { IERC20 } from "@openzeppelin/contracts-0.8/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts-0.8/token/ERC20/utils/SafeERC20.sol";
 
+/**
+ * @title AuraPenaltyForwarder
+ * @dev Receives a given token and forwards it on to a distribution contract.
+ */
+contract AuraPenaltyForwarder {
+    using SafeERC20 for IERC20;
+
+    IExtraRewardsDistributor public immutable distributor;
+    IERC20 public immutable token;
+
+    uint256 public immutable distributionDelay;
+    uint256 public lastDistribution;
+
+    event Forwarded(uint256 amount);
+
+    constructor(
+        address _distributor,
+        address _token,
+        uint256 _delay
+    ) {
+        distributor = IExtraRewardsDistributor(_distributor);
+        token = IERC20(_token);
+        distributionDelay = _delay;
+
+        lastDistribution = block.timestamp;
+
+        token.safeApprove(address(distributor), type(uint256).max);
+    }
+
+    /**
+     * @dev Forwards the complete balance of token in this contract to the distributor
+     */
+    function forward() public {
+        require(block.timestamp > lastDistribution + distributionDelay, "!elapsed");
+        lastDistribution = block.timestamp;
+
+        uint256 bal = token.balanceOf(address(this));
+        require(bal > 0, "!empty");
+
+        distributor.addReward(address(token), bal);
+
+        emit Forwarded(bal);
+    }
 }
