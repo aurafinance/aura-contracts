5715045f3172cac0b4df86832b936398fc946a0b
0xahtle7 <100377534+0xahtle7@users.noreply.github.com>
2022-04-19 16:28:38 +0100

    test: add unit test to AuraBalRewardPool (#63)
    * test: add unit test to AuraBalRewardPool

--- test/AuraBalRewardPool.spec.ts
+++ test/AuraBalRewardPool.spec.ts
@@ -3,7 +3,7 @@ import { Signer } from "ethers";
 import { expect } from "chai";
 import { deployPhase1, deployPhase2, deployPhase3, deployPhase4, SystemDeployed } from "../scripts/deploySystem";
 import { deployMocks, getMockDistro, getMockMultisigs } from "../scripts/deployMocks";
-import { AuraBalRewardPool, ERC20, MockVoteStorage__factory } from "../types/generated";
+import { AuraBalRewardPool, AuraBalRewardPool__factory, ERC20 } from "../types/generated";
 import { ONE_DAY, ONE_WEEK, ZERO_ADDRESS } from "../test-utils/constants";
 import { increaseTime, getTimestamp } from "../test-utils/time";
 import { BN, simpleToExactAmount } from "../test-utils/math";
@@ -22,9 +22,11 @@ describe("AuraBalRewardPool", () => {
     let aliceAddress: string;
     let bob: Signer;
     let bobAddress: string;
-
+    let rob: Signer;
+    let robAddress: string;
     let initialBal: BN;
     let rewardAmount: BN;
+    let stakeAmount: BN;
 
     const reset = async () => {
         const mocks = await deployMocks(deployer);
@@ -50,6 +52,9 @@ describe("AuraBalRewardPool", () => {
         bob = accounts[2];
         bobAddress = await bob.getAddress();
 
+        rob = accounts[3];
+        robAddress = await rob.getAddress();
+
         rewards = contracts.initialCvxCrvStaking.connect(alice);
         cvxCrv = contracts.cvxCrv.connect(alice) as ERC20;
 
@@ -59,8 +64,42 @@ describe("AuraBalRewardPool", () => {
         await contracts.crvDepositor.connect(alice)["deposit(uint256,bool,address)"](initialBal, true, ZERO_ADDRESS);
         initialBal = initialBal.div(2);
         await cvxCrv.transfer(bobAddress, initialBal);
-    };
 
+        stakeAmount = initialBal.div(5);
+    };
+    async function verifyWithdraw(signer: Signer, accountAddress: string, amount: BN, claim = false, lock = false) {
+        const totalSupplyBefore = await rewards.totalSupply();
+        const stakedBalanceBefore = await rewards.balanceOf(accountAddress);
+        const lockedBalanceBefore = await contracts.cvxLocker.balances(accountAddress);
+        const stakedTknBalanceBefore = await cvxCrv.balanceOf(accountAddress);
+        const cvxBalanceBefore = await contracts.cvx.balanceOf(accountAddress);
+        const pendingPenaltyBefore = await rewards.pendingPenalty();
+
+        // Test withdraw(amount,claim, lock)
+        const tx = await rewards.connect(signer).withdraw(amount, claim, lock);
+        await expect(tx).to.emit(rewards, "Withdrawn").withArgs(accountAddress, amount);
+        const pendingPenaltyAfter = await rewards.pendingPenalty();
+        const lockedBalanceAfter = await contracts.cvxLocker.balances(accountAddress);
+
+        // expect to update reward
+        expect(await rewards.balanceOf(accountAddress)).eq(stakedBalanceBefore.sub(amount));
+        expect(await rewards.totalSupply()).eq(totalSupplyBefore.sub(amount));
+        expect(await cvxCrv.balanceOf(accountAddress)).eq(stakedTknBalanceBefore.add(amount));
+        if (claim) {
+            expect(await rewards.rewards(accountAddress)).eq(0);
+            //  rewards[account] is updated twice, at withdraw and at getReward so we can't check it directly.
+            if (lock) {
+                expect(lockedBalanceAfter.locked.gt(lockedBalanceBefore.locked), "locked balance should increase");
+                expect(pendingPenaltyAfter, "no penalty").eq(pendingPenaltyBefore);
+            } else {
+                const cvxBalanceAfter = await contracts.cvx.balanceOf(accountAddress);
+                const pendingPenalty = pendingPenaltyAfter.sub(pendingPenaltyBefore);
+                // The amount CVX send to the user is 4 times the penalty, ie: rewards to user = earned 80%, penalty = earned 20%
+                assertBNClosePercent(cvxBalanceAfter.sub(cvxBalanceBefore), pendingPenalty.mul(4), "0.001");
+                assertBNClosePercent(await rewards.pendingPenalty(), pendingPenalty, "0.001");
+            }
+        }
+    }
     before(async () => {
         accounts = await ethers.getSigners();
         deployer = accounts[0];
@@ -81,8 +120,8 @@ describe("AuraBalRewardPool", () => {
     });
     describe("basic flow", () => {
         it("allows users to deposit before rewards are added (no rewards accrued)", async () => {
-            await cvxCrv.approve(rewards.address, initialBal.div(5));
-            await rewards.stake(initialBal.div(5));
+            await cvxCrv.approve(rewards.address, stakeAmount);
+            await rewards.stake(stakeAmount);
             expect(await rewards.rewardPerTokenStored()).eq(0);
         });
         it("allows anyone to trigger rewards distribution after startTime", async () => {
@@ -100,18 +139,28 @@ describe("AuraBalRewardPool", () => {
         it("accrues rewards to existing depositors following startTime", async () => {
             await increaseTime(ONE_WEEK.div(5));
             const balBefore = await contracts.cvxLocker.balances(aliceAddress);
-            await rewards.getReward(true);
+            await rewards.getReward(true); // no penalty
             const balAfter = await contracts.cvxLocker.balances(aliceAddress);
             assertBNClosePercent(balAfter.locked.sub(balBefore.locked), rewardAmount.div(10), "0.01");
         });
         it("allows subsequent deposits", async () => {
-            await cvxCrv.connect(bob).approve(rewards.address, initialBal.div(5));
-            await rewards.connect(bob).stake(initialBal.div(5));
+            await cvxCrv.connect(bob).approve(rewards.address, stakeAmount);
+            await rewards.connect(bob).stake(stakeAmount);
+        });
+        it("allows users to stake For someone else", async () => {
+            await cvxCrv.connect(bob).approve(rewards.address, stakeAmount);
+            const stakedBalanceBefore = await rewards.balanceOf(robAddress);
+            const totalSupplyBefore = await rewards.totalSupply();
+            await expect(rewards.connect(bob).stakeFor(robAddress, stakeAmount))
+                .to.emit(rewards, "Staked")
+                .withArgs(robAddress, stakeAmount);
+            expect(await rewards.balanceOf(robAddress)).eq(stakedBalanceBefore.add(stakeAmount));
+            expect(await rewards.totalSupply()).eq(totalSupplyBefore.add(stakeAmount));
         });
         it("penalises claimers who do not lock", async () => {
             await increaseTime(ONE_WEEK.div(5));
             const earned = await rewards.earned(bobAddress);
-            assertBNClosePercent(earned, rewardAmount.div(20), "0.01");
+            assertBNClosePercent(earned, rewardAmount.div(30), "0.01");
 
             const balBefore = await contracts.cvx.balanceOf(bobAddress);
             await rewards.connect(bob).getReward(false);
@@ -124,7 +173,7 @@ describe("AuraBalRewardPool", () => {
             const balBefore = await contracts.cvxLocker.balances(aliceAddress);
             await rewards.getReward(true);
             const balAfter = await contracts.cvxLocker.balances(aliceAddress);
-            assertBNClosePercent(balAfter.locked.sub(balBefore.locked), rewardAmount.div(20), "0.01");
+            assertBNClosePercent(balAfter.locked.sub(balBefore.locked), rewardAmount.div(30), "0.01");
         });
         it("allows anyone to forward penalty on to the PenaltyForwarder", async () => {
             const penalty = await rewards.pendingPenalty();
@@ -139,6 +188,30 @@ describe("AuraBalRewardPool", () => {
             await rewards.forwardPenalty();
             const balAfter = await contracts.cvx.balanceOf(rewards.address);
             expect(balAfter).eq(balBefore);
+        });
+        it("allows users to stakeAll", async () => {
+            const bobCvxCrvBalance = await cvxCrv.balanceOf(bobAddress);
+
+            await cvxCrv.connect(bob).approve(rewards.address, bobCvxCrvBalance);
+            const stakedBalanceBefore = await rewards.balanceOf(bobAddress);
+            const totalSupplyBefore = await rewards.totalSupply();
+            await expect(rewards.connect(bob).stakeAll())
+                .to.emit(rewards, "Staked")
+                .withArgs(bobAddress, bobCvxCrvBalance);
+            expect(await rewards.balanceOf(bobAddress)).eq(stakedBalanceBefore.add(bobCvxCrvBalance));
+            expect(await rewards.totalSupply()).eq(totalSupplyBefore.add(bobCvxCrvBalance));
+        });
+        it("allows users to withdraw", async () => {
+            // no reward claim , no stake
+            await verifyWithdraw(bob, bobAddress, stakeAmount, false, false);
+        });
+        it("allows users to withdraw and claim rewards", async () => {
+            // Withdraw and claim rewards with penalty
+            await verifyWithdraw(bob, bobAddress, stakeAmount, true, false);
+        });
+        it("allows users to withdraw and stake rewards", async () => {
+            // Withdraw, claim rewards and stake them to avoid penalty
+            await verifyWithdraw(bob, bobAddress, stakeAmount, true, true);
         });
     });
     describe("funding rewards", () => {
@@ -155,6 +228,42 @@ describe("AuraBalRewardPool", () => {
         it("only allows funding to be called once, ever", async () => {
             await increaseTime(ONE_WEEK);
             await expect(rewards.initialiseRewards()).to.be.revertedWith("!one time");
+        });
+        it("blocks funding if the pool has no balance", async () => {
+            const rewardPool = await new AuraBalRewardPool__factory(deployer).deploy(
+                cvxCrv.address,
+                contracts.cvx.address,
+                await deployer.getAddress(),
+                contracts.cvxLocker.address,
+                contracts.penaltyForwarder.address,
+                ONE_WEEK,
+            );
+            await expect(rewardPool.connect(deployer).initialiseRewards()).to.be.revertedWith("!balance");
+        });
+    });
+    describe("fails", () => {
+        it("if stake amount is zero", async () => {
+            await expect(rewards.connect(bob).stake(0)).to.revertedWith("RewardPool : Cannot stake 0");
+        });
+        it("if stake for amount is zero", async () => {
+            await expect(rewards.connect(bob).stakeFor(robAddress, 0)).to.revertedWith("RewardPool : Cannot stake 0");
+        });
+        it("if users to stake for does not exist", async () => {
+            await expect(rewards.connect(bob).withdraw(0, true, true)).to.revertedWith(
+                "RewardPool : Cannot withdraw 0",
+            );
+        });
+        it("constructor pass wrong arguments", async () => {
+            await expect(
+                new AuraBalRewardPool__factory(deployer).deploy(
+                    cvxCrv.address,
+                    contracts.cvx.address,
+                    await deployer.getAddress(),
+                    contracts.cvxLocker.address,
+                    contracts.penaltyForwarder.address,
+                    ONE_WEEK.mul(2),
+                ),
+            ).revertedWith("!delay");
         });
     });
 });
