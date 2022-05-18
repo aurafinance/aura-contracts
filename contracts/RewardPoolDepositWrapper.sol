2bb5b2dc0161dbaefc52180881f11a2e004b4bfc
0xmaharishi <cm972646@gmail.com>
2022-05-18 10:09:05 +0100

    feat: deposit wrapper

---
+++ contracts/RewardPoolDepositWrapper.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import { IVault } from "./Interfaces.sol";
+import { IERC20 } from "@openzeppelin/contracts-0.8/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts-0.8/token/ERC20/utils/SafeERC20.sol";
+
+interface RewardPool {
+    function deposit(uint256 assets, address receiver) external returns (uint256 shares);
+}
+
+contract RewardPoolDepositWrapper {
+    using SafeERC20 for IERC20;
+
+    IVault public immutable bVault;
+
+    constructor(address _bVault) {
+        bVault = IVault(_bVault);
+    }
+
+    function depositSingle(
+        address _rewardPoolAddress,
+        IERC20 _inputToken,
+        uint256 _inputAmount,
+        bytes32 _balancerPoolId,
+        IVault.JoinPoolRequest memory _request
+    ) public {
+        // 1. Transfer input token
+        _inputToken.safeTransferFrom(msg.sender, address(this), _inputAmount);
+
+        // 2. Deposit to balancer pool
+        (address pool, ) = bVault.getPool(_balancerPoolId);
+
+        _inputToken.approve(address(bVault), _inputAmount);
+        bVault.joinPool(_balancerPoolId, address(this), address(this), _request);
+
+        uint256 minted = IERC20(pool).balanceOf(address(this));
+        require(minted > 0, "!mint");
+
+        uint256 inputBalAfter = _inputToken.balanceOf(address(this));
+        require(inputBalAfter == 0, "!input");
+
+        // 3. Deposit to reward pool
+        IERC20(pool).approve(_rewardPoolAddress, minted);
+        RewardPool(_rewardPoolAddress).deposit(minted, msg.sender);
+    }
+}
