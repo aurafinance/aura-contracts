f70def75176fd4c3f44154be37bab869fbc65b99
0xMaharishi <cm972646@gmail.com>
2022-06-09 09:38:00 +0100

    chore: add missing file

---
+++ contracts/mocks/balancer/MockLBPFactory.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import "@openzeppelin/contracts-0.8/token/ERC20/IERC20.sol";
+
+interface ILBPFactory {
+    function create(
+        string memory name,
+        string memory symbol,
+        IERC20[] memory tokens,
+        uint256[] memory weights,
+        uint256 swapFeePercentage,
+        address owner,
+        bool swapEnabledOnStart
+    ) external returns (address);
+}
+
+interface ILBP {
+    function setSwapEnabled(bool swapEnabled) external;
+
+    function updateWeightsGradually(
+        uint256 startTime,
+        uint256 endTime,
+        uint256[] memory endWeights
+    ) external;
+}
