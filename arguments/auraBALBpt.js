c2fc6b22030c2ad2299a97132f04c4b700ba601f
0xahtle7 <0xahtle7@gmail.com>
2022-06-09 15:14:08 +0100

    chore: adds arguments/readme

---
+++ arguments/auraBALBpt.js
@@ -0,0 +1,1 @@
+module.exports = ["0xaF52695E1bB01A16D33D7194C28C42b10e0Dbec2", "Aura", "AURA"];
---
+++ arguments/voterProxy.js
@@ -0,0 +1,8 @@
+module.exports = [
+    "0x59A5ccD34943CD0AdCf5ce703EE9F06889E13707",
+    "0xC0c293ce456fF0ED870ADd98a0828Dd4d2903DBF",
+    "0x6fc73b9d624b543f8b6b88fc3ce627877ff169ee",
+    "0x5bd3fCA8D3d8c94a6419d85E0a76ec8Da52d836a",
+];
+
+// [extSystem.minter, extSystem.token, extSystem.tokenBpt, extSystem.votingEscrow, extSystem.gaugeController],
