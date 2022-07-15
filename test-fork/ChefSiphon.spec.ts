ea69f6dfb1fde1787f1afa7b475487e150c6343b
phijfry <phijfry@protonmail.com>
2022-07-15 12:59:56 +0100

    feat: neaten up tests

--- test-fork/ChefSiphon.spec.ts
+++ test-fork/ChefSiphon.spec.ts
@@ -1,39 +1,42 @@
 import hre, { ethers, network } from "hardhat";
+import { Signer } from "ethers";
 import { expect } from "chai";
 import {
     ConvexMasterChef,
-    ConvexMasterChef__factory,
-    ERC20__factory,
     SiphonToken,
     SiphonToken__factory,
     ChefForwarder,
     ChefForwarder__factory,
     MasterChefRewardHook,
     MasterChefRewardHook__factory,
-    BoosterOwner__factory,
-    Booster__factory,
     ExtraRewardStashV3__factory,
+    AuraToken,
+    BoosterOwner,
+    Booster,
 } from "../types/generated";
 import { advanceBlock, impersonateAccount } from "../test-utils";
-import { Signer } from "ethers";
 import { deployContract } from "../tasks/utils";
+import { parseUnits } from "ethers/lib/utils";
+import { config } from "../tasks/deploy/mainnet-config";
+import { Phase2Deployed } from "scripts/deploySystem";
 
 const debug = false;
 
-const auraToken = "0xC0c293ce456fF0ED870ADd98a0828Dd4d2903DBF";
-const masterChefAdress = "0x1ab80F7Fb46B25b7e0B2cfAC23Fc88AC37aaf4e9";
-const chefAdminAddress = "0x5fea4413e3cc5cf3a29a49db41ac0c24850417a0";
-const briberAddress = "0x3000d9B2c0E6B9F97f30ABE379eaAa8A85A04afC";
+const protocolMultisig = "0x5fea4413e3cc5cf3a29a49db41ac0c24850417a0";
+const eoaAddress = "0x3000d9B2c0E6B9F97f30ABE379eaAa8A85A04afC";
 const stashAddress = "0xF801a238a1Accc7A63b429E8c343B198d51fbbb9";
-const boosterOwnerAddress = "0xFa838Af70314135159b309bf27f1DbF1F954eC34";
-const boosterAddress = "0x7818A1DA7BD1E64c199029E86Ba244a9798eEE10";
 const zeroAddress = "0x0000000000000000000000000000000000000000";
 const auraBalPid = 19;
 
 describe("ChefSiphon", () => {
-    let chefAdmin: Signer;
-    let briber: Signer;
+    let protocolDao: Signer;
+    let eoa: Signer;
+    let phase2: Phase2Deployed;
+
     let masterChef: ConvexMasterChef;
+    let auraToken: AuraToken;
+    let boosterOwner: BoosterOwner;
+    let booster: Booster;
 
     before(async () => {
         await network.provider.request({
@@ -48,111 +51,133 @@ describe("ChefSiphon", () => {
             ],
         });
 
-        await impersonateAccount(chefAdminAddress);
-        chefAdmin = await ethers.getSigner(chefAdminAddress);
+        await impersonateAccount(protocolMultisig);
+        protocolDao = await ethers.getSigner(protocolMultisig);
 
-        await impersonateAccount(briberAddress);
-        briber = await ethers.getSigner(briberAddress);
+        await impersonateAccount(eoaAddress);
+        eoa = await ethers.getSigner(eoaAddress);
 
-        masterChef = ConvexMasterChef__factory.connect(masterChefAdress, chefAdmin);
+        phase2 = await config.getPhase2(protocolDao);
+        masterChef = phase2.chef;
+        auraToken = phase2.cvx;
+        boosterOwner = phase2.boosterOwner;
+        booster = phase2.booster;
     });
 
     describe("divert chef rewards", () => {
-        const mintAmount = 1;
-
-        const siphonTokens: SiphonToken[] = [];
         let masterChefRewardHook: MasterChefRewardHook;
+        let masterChefRewardHookSiphonToken: SiphonToken;
+
         let chefForwarder: ChefForwarder;
+        let chefForwarderSiphonToken: SiphonToken;
+
+        // -------------------------------------------------------------------
+        // EOA will perform these actions
+        // -------------------------------------------------------------------
+
+        const mintAmount = parseUnits("1");
 
         it("deploy ChefForwarder", async () => {
-            chefForwarder = await deployContract<ChefForwarder>(
+            chefForwarder = await deployContract<ChefForwarder>(hre, new ChefForwarder__factory(eoa), "ChefForwarder", [
+                masterChef.address,
+            ]);
+            await chefForwarder.setBriber(eoaAddress);
+        });
+        it("deploy siphon tokens for chefForwarder", async () => {
+            chefForwarderSiphonToken = await deployContract<SiphonToken>(
                 hre,
-                new ChefForwarder__factory(chefAdmin),
-                "ChefForwarder",
-                [masterChefAdress],
+                new SiphonToken__factory(eoa),
+                "SiphonTokenBribes",
+                [chefForwarder.address, mintAmount],
+                {},
+                debug,
             );
-            await chefForwarder.setBriber(briberAddress);
+            const balance = await chefForwarderSiphonToken.balanceOf(chefForwarder.address);
+            expect(balance).eq(mintAmount);
         });
         it("deploy MasterChefRewardHook", async () => {
             masterChefRewardHook = await deployContract<MasterChefRewardHook>(
                 hre,
-                new MasterChefRewardHook__factory(chefAdmin),
+                new MasterChefRewardHook__factory(eoa),
                 "MasterChefRewardHook",
-                [stashAddress, masterChefAdress, auraToken],
+                [stashAddress, masterChef.address, auraToken.address],
             );
         });
-        it("deploy chef siphon tokens", async () => {
-            for (const rewardHandler of [chefForwarder, masterChefRewardHook]) {
-                const factory = new SiphonToken__factory(chefAdmin);
-                const token = await deployContract<SiphonToken>(
-                    hre,
-                    factory,
-                    "SiphonTokenBribes",
-                    [rewardHandler.address, mintAmount],
-                    {},
-                    debug,
-                );
-                const balance = await token.balanceOf(rewardHandler.address);
-                expect(balance).eq(mintAmount);
-                siphonTokens.push(token);
-            }
+        it("deploy siphon tokens for masterChefRewardHook", async () => {
+            masterChefRewardHookSiphonToken = await deployContract<SiphonToken>(
+                hre,
+                new SiphonToken__factory(eoa),
+                "SiphonTokenBribes",
+                [masterChefRewardHook.address, mintAmount],
+                {},
+                debug,
+            );
+            const balance = await masterChefRewardHookSiphonToken.balanceOf(masterChefRewardHook.address);
+            expect(balance).eq(mintAmount);
         });
-        it("stop rewards for pid:0", async () => {
+        it("transfer ownership of reward contracts to protocolMultisig", async () => {
+            await masterChefRewardHook.transferOwnership(protocolMultisig);
+            await chefForwarder.transferOwnership(protocolMultisig);
+
+            masterChefRewardHook = masterChefRewardHook.connect(protocolDao);
+            chefForwarder = chefForwarder.connect(protocolDao);
+        });
+
+        // -------------------------------------------------------------------
+        // Protocol DAO will perform these actions
+        // -------------------------------------------------------------------
+        const poolAllocPoint = "1000";
+        const rewarder = zeroAddress;
+
+        it("add siphon token for masterChefRewardHook", async () => {
+            const pid = await masterChef.poolLength();
+            await masterChefRewardHook.setPid(pid);
+            await masterChef.add(poolAllocPoint, masterChefRewardHookSiphonToken.address, rewarder);
+            expect(await masterChef.isAddedPool(masterChefRewardHookSiphonToken.address)).eq(true);
+        });
+        it("add siphon token for chefForwarder", async () => {
+            const pid = await masterChef.poolLength();
+            await chefForwarder.setPid(pid);
+            await masterChef.add(poolAllocPoint, chefForwarderSiphonToken.address, rewarder);
+            expect(await masterChef.isAddedPool(chefForwarderSiphonToken.address)).eq(true);
+        });
+        it("set rewards for pid 0 and 1 to 0", async () => {
             await masterChef.set(0, 0, zeroAddress, false);
+            await masterChef.set(1, 0, zeroAddress, false);
         });
-        it("add siphon token to chef", async () => {
-            const poolAllocPoint = "1000";
-            const rewarder = zeroAddress;
-            const rewardHandlers = [chefForwarder, masterChefRewardHook];
-
-            for (let i = 0; i < siphonTokens.length; i++) {
-                const pid = await masterChef.poolLength();
-                await rewardHandlers[i].setPid(pid);
-                await masterChef.add(poolAllocPoint, siphonTokens[i].address, rewarder);
-                expect(await masterChef.isAddedPool(siphonTokens[i].address)).eq(true);
-            }
+        it("deposit tokens for chefForwarder and masterChefRewardHook", async () => {
+            await chefForwarder.deposit(chefForwarderSiphonToken.address);
+            await masterChefRewardHook.deposit(masterChefRewardHookSiphonToken.address);
         });
-        it("deposit tokens", async () => {
-            await chefForwarder.deposit(siphonTokens[0].address);
-            await masterChefRewardHook.deposit(siphonTokens[1].address);
-        });
-        it("claim rewards for ChefForwarder", async () => {
-            const rewardTokenAddress = await masterChef.cvx();
-            const rewardToken = ERC20__factory.connect(rewardTokenAddress, briber);
+        it("set stash reward hook to masterChefRewardHook", async () => {
+            await boosterOwner.setStashExtraReward(stashAddress, auraToken.address);
+            await boosterOwner.setStashRewardHook(stashAddress, masterChefRewardHook.address);
 
-            const balanceBefore = await rewardToken.balanceOf(briberAddress);
-
-            await advanceBlock();
-
-            await chefForwarder.connect(briber).claim(rewardTokenAddress);
-            const balanceAfter = await rewardToken.balanceOf(briberAddress);
-
-            expect(balanceAfter).gt(balanceBefore);
-        });
-        it("set extra rewards on stash", async () => {
-            const boosterOwner = BoosterOwner__factory.connect(boosterOwnerAddress, chefAdmin);
-            const admin = await boosterOwner.owner();
-            await impersonateAccount(admin);
-            const adminSigner = await ethers.getSigner(admin);
-            await boosterOwner.connect(adminSigner).setStashExtraReward(stashAddress, auraToken);
-            await boosterOwner.connect(adminSigner).setStashRewardHook(stashAddress, masterChefRewardHook.address);
-
-            const stash = ExtraRewardStashV3__factory.connect(stashAddress, chefAdmin);
+            const stash = ExtraRewardStashV3__factory.connect(stashAddress, protocolDao);
             expect(await stash.rewardHook()).eq(masterChefRewardHook.address);
         });
+
+        // Test claiming works
+        it("claim rewards for ChefForwarder", async () => {
+            const balanceBefore = await auraToken.balanceOf(eoaAddress);
+            await advanceBlock();
+
+            await chefForwarder.connect(eoa).claim(auraToken.address);
+            const balanceAfter = await auraToken.balanceOf(eoaAddress);
+            expect(balanceAfter).gt(balanceBefore);
+        });
         it("claim rewards via earmarkRewards for MasterChefRewardHook", async () => {
-            const rewardTokenAddress = await masterChef.cvx();
-            const rewardToken = ERC20__factory.connect(rewardTokenAddress, chefAdmin);
-            const booster = Booster__factory.connect(boosterAddress, chefAdmin);
             const poolInfo = await booster.poolInfo(auraBalPid);
             expect(poolInfo.stash).eq(stashAddress);
-            const stash = ExtraRewardStashV3__factory.connect(stashAddress, chefAdmin);
-            const tokenInfo = await stash.tokenInfo(rewardTokenAddress);
 
-            const balanceBefore = await rewardToken.balanceOf(tokenInfo.rewardAddress);
+            const stash = ExtraRewardStashV3__factory.connect(stashAddress, protocolDao);
+            const tokenInfo = await stash.tokenInfo(auraToken.address);
+
+            const balanceBefore = await auraToken.balanceOf(tokenInfo.rewardAddress);
             await advanceBlock();
             await booster.earmarkRewards(auraBalPid);
-            const balanceAfter = await rewardToken.balanceOf(tokenInfo.rewardAddress);
+            const balanceAfter = await auraToken.balanceOf(tokenInfo.rewardAddress);
+
             expect(balanceAfter).gt(balanceBefore);
         });
     });
