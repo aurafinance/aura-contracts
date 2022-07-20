97cae18c7f32e949b27edebcb098b4572332f119
phijfry <phijfry@protonmail.com>
2022-07-20 10:36:56 +0100

    feat: WIP fee distro update fork test

--- test-fork/FeeDistroUpdate.spec.ts
+++ test-fork/FeeDistroUpdate.spec.ts
@@ -1,18 +1,30 @@
-import hre, { ethers, network } from "hardhat";
+import { ethers, network } from "hardhat";
 import { expect } from "chai";
-import { BoosterOwner__factory, Booster__factory } from "../types/generated";
-import { impersonateAccount } from "../test-utils";
+import {
+    Booster,
+    BoosterOwner,
+    IERC20,
+    IERC20__factory,
+    IFeeDistributor,
+    IFeeDistributor__factory,
+    VoterProxy,
+} from "../types/generated";
+import { impersonateAccount, simpleToExactAmount } from "../test-utils";
 import { Signer } from "ethers";
-import { deployContract } from "../tasks/utils";
+import { config } from "../tasks/deploy/mainnet-config";
+import { _TypedDataEncoder } from "ethers/lib/utils";
 
-const debug = false;
-
-const boosterOwnerAddress = "0xFa838Af70314135159b309bf27f1DbF1F954eC34";
-const boosterAddress = "0x7818A1DA7BD1E64c199029E86Ba244a9798eEE10";
-const zeroAddress = "0x0000000000000000000000000000000000000000";
 const newFeeDistro = "0xD3cf852898b21fc233251427c2DC93d3d604F3BB";
 
 describe("FeeDistroUpdate", () => {
+    let protocolDao: Signer;
+    let boosterOwner: BoosterOwner;
+    let distributor: IFeeDistributor;
+    let voterProxy: VoterProxy;
+    let bal: IERC20;
+    let feeToken: IERC20;
+    let booster: Booster;
+
     before(async () => {
         await network.provider.request({
             method: "hardhat_reset",
@@ -20,23 +32,52 @@ describe("FeeDistroUpdate", () => {
                 {
                     forking: {
                         jsonRpcUrl: process.env.NODE_URL,
-                        blockNumber: 15135072,
+                        blockNumber: 15178682,
                     },
                 },
             ],
         });
+
+        await impersonateAccount(config.multisigs.daoMultisig);
+        protocolDao = await ethers.getSigner(config.multisigs.daoMultisig);
+
+        const phase2 = await config.getPhase2(protocolDao);
+        boosterOwner = phase2.boosterOwner;
+        voterProxy = phase2.voterProxy;
+        booster = phase2.booster;
+
+        distributor = IFeeDistributor__factory.connect(newFeeDistro, protocolDao);
+        bal = IERC20__factory.connect(config.addresses.token, protocolDao);
+        feeToken = IERC20__factory.connect(config.addresses.feeToken, protocolDao);
     });
 
     describe("update fee distro", () => {
+        it("deposit tokens into new feeDistro", async () => {
+            const balWhaleAddress = "0xcEacc82ddCdB00BFE19A9D3458db3e6b8aEF542B";
+
+            await impersonateAccount(balWhaleAddress);
+            await impersonateAccount(config.addresses.feeTokenWhale);
+
+            const balWhale = await ethers.getSigner(balWhaleAddress);
+            const feeWhale = await ethers.getSigner(config.addresses.feeTokenWhale);
+
+            const amount = simpleToExactAmount(100);
+
+            await bal.connect(balWhale).approve(distributor.address, amount);
+            await feeToken.connect(feeWhale).approve(distributor.address, amount);
+
+            await distributor.connect(balWhale).depositToken(config.addresses.token, amount);
+            await distributor.connect(feeWhale).depositToken(config.addresses.feeToken, amount);
+        });
         it("update fee distro contracts", async () => {
-            await booster.setFeeInfo(balToken, feeDistro);
-            await booster.setFeeInfo(bbUsd, feeDistro);
+            await boosterOwner.setFeeInfo(config.addresses.token, newFeeDistro);
+            await boosterOwner.setFeeInfo(config.addresses.feeToken, newFeeDistro);
         });
         it("set hash on voter proxy for claim", async () => {
             const domain = {
                 name: "FeeDistributor",
                 version: "1",
-                chainId: "1",
+                chainId: (await distributor.provider.getNetwork()).chainId,
                 verifyingContract: distributor.address,
             };
 
@@ -54,17 +95,23 @@ describe("FeeDistroUpdate", () => {
                 nonce: (await distributor.getNextNonce(voterProxy.address)).toString(),
             };
 
+            const voteDelegateAddress = await booster.voteDelegate();
+            await impersonateAccount(voteDelegateAddress);
+            const voteDelegate = await ethers.getSigner(voteDelegateAddress);
+
             const hash = _TypedDataEncoder.hash(domain, types, values);
+            await booster.connect(voteDelegate).setVote(hash, true);
 
-            await voterProxy.setHash(hash, true);
-            await distributor.connect(voterProxyAdmin).setOnlyCallerCheckWithSignature(voterProxy.address, true, "0x");
+            const isValid = await voterProxy.isValidSignature(hash, "0x");
+            expect(isValid).eq("0x1626ba7e");
 
-            const isValid = await voterProxy.isValidSignature(hash, "");
-
-            expect(isValid).eq("...");
+            await distributor.setOnlyCallerCheckWithSignature(voterProxy.address, true, "0x");
         });
-        it("add rewards to fee distro and fast forward 1 week", async () => {});
-        it("only voter proxy can claim rewards", async () => {});
-        it("claim rewards via earmarkRewards", async () => {});
+        xit("add rewards to fee distro and fast forward 1 week", async () => {});
+        xit("only voter proxy can claim rewards", async () => {
+            const resp = distributor.claimToken(voterProxy.address, config.addresses.token);
+            expect(resp).to.be.revertedWith("fucked");
+        });
+        xit("claim rewards via earmarkRewards", async () => {});
     });
 });
