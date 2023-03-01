7a506d3ddc48660a4046dd8b631c3620064d236e
phijfry <phijfry@protonmail.com>
2023-03-01 23:41:20 +0000

    feat: refactor snapshot tasks

---
+++ tasks/snapshot/create.ts
@@ -0,0 +1,171 @@
+import * as fs from "fs";
+import * as path from "path";
+import { Wallet } from "ethers";
+import { task } from "hardhat/config";
+import { TaskArguments } from "hardhat/types";
+import snapshot from "@snapshot-labs/snapshot.js";
+
+import { getGaugeChoices, ordinalSuffix, GaugeChoice } from "./utils";
+import { HardhatRuntime } from "../utils/networkAddressFactory";
+import { configs, months } from "./constants";
+import { createInterface } from "readline";
+import { uniq } from "lodash";
+
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+task("snapshot:create")
+    .addParam("snapshot", "The block to snapshot")
+    .addParam("hub", "test or main config")
+    .setAction(async function (taskArgs: TaskArguments, hre: HardhatRuntime) {
+        const config = configs[taskArgs.hub];
+
+        if (!config) {
+            console.log("Invalid config value:", taskArgs.config);
+            return;
+        }
+
+        const wallet = new Wallet(process.env.PRIVATE_KEY);
+        const account = wallet.address;
+
+        const networkName = hre.network.name;
+        if (networkName !== "mainnet") {
+            console.log(`Invalid network ID. Found ${networkName} Expecting mainnet`);
+            return;
+        }
+
+        // get gauges
+        console.log("Getting gauges choices");
+        const gaugeList = getGaugeChoices();
+        console.log("Gauge list:");
+        gaugeList.forEach((gauge: GaugeChoice, i: number) => console.log(`${i + 1}) ${gauge.label}`));
+
+        // create proposal
+        console.log("Creating proposal on snapshot");
+        const latestBlock = taskArgs.snapshot;
+        if (!latestBlock) {
+            console.log(`Invalid snashot provided. Found ${snapshot}`);
+        }
+        const client = new snapshot.Client712(config.hub);
+
+        const space = config.space;
+
+        const localDate = new Date();
+
+        const startDate = new Date(
+            Date.UTC(localDate.getUTCFullYear(), localDate.getUTCMonth(), localDate.getUTCDate()),
+        );
+        const dayCurr = startDate.getDay();
+        const dayTarget = 4; // Thursday
+        const dayDelta = dayTarget - dayCurr;
+        startDate.setDate(startDate.getDate() + dayDelta);
+        startDate.setUTCHours(2);
+        startDate.setUTCMinutes(0);
+
+        const validStartDate = startDate.toUTCString().startsWith("Thu");
+        if (!validStartDate) {
+            console.log("Invalid start date:", startDate.toUTCString());
+            console.log("Voting should start on a Thursday");
+            return;
+        }
+        console.log("Start date:", startDate.toUTCString());
+
+        const endDate = new Date(Date.UTC(startDate.getUTCFullYear(), startDate.getUTCMonth(), startDate.getUTCDate()));
+        endDate.setUTCDate(endDate.getUTCDate() + 5);
+        endDate.setUTCHours(2);
+        endDate.setUTCMinutes(0);
+
+        const validEndDate = endDate.toUTCString().startsWith("Tue");
+        if (!validEndDate) {
+            console.log("Invalid end date:", endDate.toUTCString());
+            console.log("Voting should end on a Tuesday");
+            return;
+        }
+        console.log("End date:", endDate.toUTCString());
+        console.log(`Snapshot: ${latestBlock}`);
+        console.log(`Space: ${space}`);
+        console.log(`Account: ${account}`);
+        console.log(`Hub: ${config.hub}`);
+
+        const title = `Gauge Weight for Week of ${ordinalSuffix(startDate.getUTCDate())} ${
+            months[startDate.getUTCMonth()]
+        } ${startDate.getUTCFullYear()}`;
+        console.log(`Title: ${title}`);
+
+        const body =
+            "Please read gauge voting rules before voting: https://docs.aura.finance/aura/governance/gauge-voting#gauge-voting-rules-and-information\n\nBe sure to also consult the voting dashboard for gauge voting insights: https://app.aura.finance/#/lock";
+        console.log("Body:", body);
+
+        const choices = gaugeList.map((gauge: GaugeChoice) => gauge.label);
+        if (choices.length !== uniq(choices).length) {
+            choices.forEach((choice: string) => {
+                const count = choices.filter((c: string) => c === choice).length;
+                if (count > 1) console.log("Duplicate:", choice);
+            });
+            console.log("Duplicate labels not allowed");
+            return;
+        }
+
+        await new Promise(res => {
+            readline.question(`Do you want to submit this proposal [y/n]: `, async answer => {
+                if (answer.toLowerCase() === "y") {
+                    console.log("Submitting to snapshot hub");
+                    try {
+                        const start = Math.floor(startDate.getTime() / 1000);
+                        const end = Math.floor(endDate.getTime() / 1000);
+                        const snapshot = Number(latestBlock);
+
+                        const proposal = {
+                            space,
+                            type: "weighted",
+                            title,
+                            body,
+                            discussion: "",
+                            choices,
+                            start,
+                            end,
+                            snapshot,
+                            network: "1",
+                            strategies: JSON.stringify({}),
+                            plugins: JSON.stringify({}),
+                            metadata: JSON.stringify({}),
+                        };
+                        console.log("Proposal:", JSON.stringify(proposal));
+                        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                        const receipt: any = await client.proposal(wallet, account, proposal);
+
+                        console.log(receipt);
+
+                        // Save the proposal receipt to proposal.json file
+                        const savedProposalPath = path.resolve(__dirname, "./proposals.json");
+                        const saved = fs.readFileSync(savedProposalPath, "utf8");
+                        const savedJSON = JSON.parse(saved);
+                        const newSaved = [
+                            {
+                                id: receipt.id,
+                                title,
+                                start,
+                                end,
+                                snapshot,
+                            },
+                            ...savedJSON,
+                        ];
+                        fs.writeFileSync(savedProposalPath, JSON.stringify(newSaved));
+
+                        console.log(receipt);
+                    } catch (error) {
+                        console.log("Submitting failed");
+                        console.log(error);
+                    }
+                    readline.close();
+                    res(null);
+                } else {
+                    console.log("Cancelled");
+                    readline.close();
+                    res(null);
+                }
+            });
+        });
+    });
---
+++ tasks/snapshot/generate.ts
@@ -0,0 +1,82 @@
+import { Contract } from "ethers";
+import { task } from "hardhat/config";
+import { TaskArguments } from "hardhat/types";
+
+import {
+    getGaugeChoices,
+    getGaugeSnapshot,
+    parseLabel,
+    saveGaugeChoices,
+    sortGaugeList,
+    compareAddresses,
+    GaugeChoice,
+} from "./utils";
+import { getSigner } from "../utils";
+import { config } from "../deploy/mainnet-config";
+import { HardhatRuntime } from "../utils/networkAddressFactory";
+import { IGaugeController__factory, MockCurveGauge__factory } from "../../types";
+import { removedGauges, validNetworks } from "./constants";
+import { uniqBy } from "lodash";
+
+const gaugeFilterNetworks = (gauge: any) => validNetworks.includes(gauge.network);
+const gaugeFilterPoolType = (gauge: any) => gauge.pool.poolType !== "Element";
+const gaugeFormatRow = (gauge: any) => ({ address: gauge.address, label: parseLabel(gauge) });
+
+task("snapshot:generate").setAction(async function (_: TaskArguments, hre: HardhatRuntime) {
+    const signer = await getSigner(hre);
+    const gaugeSnapshot = getGaugeSnapshot();
+
+    const validNetworkGauges = gaugeSnapshot
+        .filter(gaugeFilterNetworks)
+        .filter(gaugeFilterPoolType)
+        .filter((gauge: any) => !gauge.isKilled);
+
+    const sortedGauges = sortGaugeList(validNetworkGauges);
+
+    const cleanedGauges = await Promise.all(
+        sortedGauges.map(async g => {
+            try {
+                const gauge = MockCurveGauge__factory.connect(g.address, signer);
+                if (await gauge.is_killed()) {
+                    return false;
+                }
+
+                if (removedGauges.includes(g.address.toLowerCase())) {
+                    return false;
+                }
+
+                //////////////////////////////////////////
+                // The gauge is valid so we return it //
+                //////////////////////////////////////////
+                return g;
+            } catch (e) {
+                console.log("Snapshot generate task error:", e, g);
+                return false;
+            }
+        }),
+    );
+
+    const formattedGauges = cleanedGauges.filter(Boolean).map(gaugeFormatRow);
+    saveGaugeChoices(uniqBy(formattedGauges, "address"));
+});
+
+task("snapshot:validate").setAction(async function (_: TaskArguments, hre: HardhatRuntime) {
+    const signer = await getSigner(hre);
+    const gauges = getGaugeChoices();
+    const gaugeController = IGaugeController__factory.connect(config.addresses.gaugeController, signer);
+
+    const count = Number((await gaugeController.n_gauges()).toString());
+
+    for (let i = 0; i < count; i++) {
+        const addr = await gaugeController.gauges(i);
+        const gauge = new Contract(addr, ["function is_killed() external view returns (bool)"], signer);
+
+        if (await gauge.is_killed()) continue;
+
+        const found = gauges.find((g: GaugeChoice) => compareAddresses(addr, g.address));
+        const isRemoved = removedGauges.find(g => compareAddresses(g, addr));
+        if (!found && !isRemoved) {
+            console.log("Missing:", i, addr);
+        }
+    }
+});
