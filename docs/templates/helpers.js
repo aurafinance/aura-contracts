d68817d56dc0ece8b38456af93b03dc621d39f59
0xahtle7 <100377534+0xahtle7@users.noreply.github.com>
2023-03-22 23:20:08 +0100

    ci: adds solidity docgen task (#191)
    * ci: adds solidity docgen task

---
+++ docs/templates/helpers.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef, @typescript-eslint/no-var-requires */
+const { findAll } = require("solidity-ast/utils");
+
+module.exports = {
+    eq: (a, b) => a === b,
+
+    /** @this {import('solidity-docgen').DocItemWithContext} */
+    allEvents() {
+        if (this.nodeType === "ContractDefinition") {
+            const { deref } = this.__item_context.build;
+            const parents = this.linearizedBaseContracts.map(deref("ContractDefinition"));
+            return parents.flatMap(p => [...findAll("EventDefinition", p)]);
+        }
+    },
+};
