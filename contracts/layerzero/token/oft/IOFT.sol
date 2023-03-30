cc385d24d5c77387e783ec072124334ca7fd5303
phijfry <phijfry@protonmail.com>
2023-03-30 16:25:08 +0100

    feat: initial sidechain commit

---
+++ contracts/layerzero/token/oft/IOFT.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.5.0;
+
+import "./IOFTCore.sol";
+import "@openzeppelin/contracts-0.8/token/ERC20/IERC20.sol";
+
+/**
+ * @dev Interface of the OFT standard
+ */
+interface IOFT is IOFTCore, IERC20 {
+
+}
