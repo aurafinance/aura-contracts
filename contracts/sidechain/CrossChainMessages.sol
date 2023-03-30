b879a2ee01b81702de46da92a92baad2479b9ae4
phijfry <phijfry@protonmail.com>
2023-03-30 23:01:14 +0100

    feat: initial scratch

--- contracts/sidechain/CrossChainMessages.sol
+++ contracts/sidechain/CrossChainMessages.sol
@@ -11,10 +11,67 @@ library CrossChainMessages {
     bytes4 public constant MAGIC_BYTES = 0x7a7f9946;
 
     enum MessageType {
-        LOCK
+        // Lock L2 AURA as vlAURA
+        LOCK,
+        // sent from the L2 to trigger a feeDebt update
+        FEES,
+        // sent from the L1 to the L2 after a successful debt update
+        // will trigger AURA to be sent to the L2 and the rate to get
+        // updated on the L2
+        FEES_CALLBACK
+    }
+
+    function getMessageType(bytes memory _payload) internal pure returns (MessageType) {
+        bytes1 messageType;
+        assembly {
+            messageType := mload(add(add(_payload, 4), 32))
+        }
+        return MessageType(uint8(messageType));
     }
 
     function isCustomMessage(bytes memory _payload) internal pure returns (bool) {
+        bytes4 sig;
+        assembly {
+            sig := mload(add(_payload, 32))
+        }
+        return sig == MAGIC_BYTES;
+    }
+
+    /* -------------------------------------------------------------------
+       Encode
+    ------------------------------------------------------------------- */
+
+    function encodeLock(address sender, uint256 amount) internal pure returns (bytes memory) {
+        // TODO:
+        return bytes("");
+    }
+
+    function encodeFees(uint256 amount) internal pure returns (bytes memory) {
+        // TODO:
+        return bytes("");
+    }
+
+    /* -------------------------------------------------------------------
+       Decode 
+    ------------------------------------------------------------------- */
+
+    function decodeFeesCallback(bytes memory _payload)
+        internal
+        pure
+        returns (
+            address,
+            uint256,
+            uint256
+        )
+    {
+        // TODO:
+    }
+
+    function decodeFees(bytes memory _payload) internal pure returns (uint256) {
+        // TODO:
+    }
+
+    function decodeLock(bytes memory _payload) internal pure returns (address, uint256) {
         // TODO:
     }
 }
