b1c78d385df8c6bd5f2b5d6665c940ac628b6a7f
phijfry <phijfry@protonmail.com>
2023-03-30 23:01:14 +0100

    feat: initial scratch

--- contracts/sidechain/Coordinator.sol
+++ contracts/sidechain/Coordinator.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.11;
 
 import { OFT } from "../layerzero/token/oft/OFT.sol";
-import { CrossChainMessages } from "./CrossChainMessages.sol";
+import { CrossChainMessages as CCM } from "./CrossChainMessages.sol";
 
 /**
  * @title Coordinator
@@ -10,14 +10,32 @@ import { CrossChainMessages } from "./CrossChainMessages.sol";
  */
 contract Coordinator is OFT {
     /* -------------------------------------------------------------------
+       Storage 
+    ------------------------------------------------------------------- */
+
+    /// @dev canonical chain ID
+    uint16 public immutable canonicalChainId;
+
+    /// @dev Booster contract
+    address public immutable booster;
+
+    /// @dev Rate to send CVX on mint
+    uint256 public mintRate;
+
+    /* -------------------------------------------------------------------
        Constructor 
     ------------------------------------------------------------------- */
 
     constructor(
         string memory _name,
         string memory _symbol,
-        address _lzEndpoint
-    ) OFT(_name, _symbol, _lzEndpoint) {}
+        address _lzEndpoint,
+        uint16 _canonicalChainId,
+        address _booster
+    ) OFT(_name, _symbol, _lzEndpoint) {
+        canonicalChainId = _canonicalChainId;
+        booster = _booster;
+    }
 
     /* -------------------------------------------------------------------
        Core Functions
@@ -29,21 +47,49 @@ contract Coordinator is OFT {
      * @param _to     Address to send CVX (L2) to
      * @param _amount Amount of CRV rewardClaimed was called with
      */
-    function mint(address _to, uint256 _amount) external {}
+    function mint(address _to, uint256 _amount) external {
+        require(msg.sender == booster, "!booster");
+        uint256 amount = (_amount * mintRate) / 1e18;
+        _transfer(address(this), _to, amount);
+    }
 
     /**
      * @dev Called by the booster.earmarkRewards to register feeDebt with the L1
      *      and receive CVX tokens in return
      * @param _rewards Amount of CRV that was received as rewards
      */
-    function queueNewRewards(uint256 _rewards) external {}
+    function queueNewRewards(uint256 _rewards, bytes memory _adapterParams) external payable {
+        bytes memory payload = CCM.encodeFees(_rewards);
+
+        _lzSend(
+            canonicalChainId, ///// Parent chain ID
+            payload, ////////////// Payload
+            payable(msg.sender), // Refund address
+            address(0), /////////// ZRO payment address
+            _adapterParams, //////// Adapter params
+            msg.value ///////////// Native fee
+        );
+    }
 
     /**
      * @dev Lock CVX on the L1 chain
      * @param _cvxAmount Amount of CVX to lock for vlCVX on L1
      * @param _adapterParams LZ adapterParams
      */
-    function lock(uint256 _cvxAmount, bytes memory _adapterParams) external payable {}
+    function lock(uint256 _cvxAmount, bytes memory _adapterParams) external payable {
+        _debitFrom(msg.sender, canonicalChainId, bytes(""), _cvxAmount);
+
+        bytes memory payload = CCM.encodeLock(msg.sender, _cvxAmount);
+
+        _lzSend(
+            canonicalChainId, ///// Parent chain ID
+            payload, ////////////// Payload
+            payable(msg.sender), // Refund address
+            address(0), /////////// ZRO payment address
+            _adapterParams, //////// Adapter params
+            msg.value ///////////// Native fee
+        );
+    }
 
     /* -------------------------------------------------------------------
       Layer Zero functions L1 -> L2
@@ -58,8 +104,20 @@ contract Coordinator is OFT {
         uint64 _nonce,
         bytes memory _payload
     ) internal virtual override {
-        if (CrossChainMessages.isCustomMessage(_payload)) {
-            // TODO:
+        if (CCM.isCustomMessage(_payload)) {
+            CCM.MessageType messageType = CCM.getMessageType(_payload);
+            if (messageType == CCM.MessageType.FEES_CALLBACK) {
+                (address toAddress, uint256 cvxAmount, uint256 crvAmount) = CCM.decodeFeesCallback(_payload);
+
+                // The mint rate is the amount of CVX we mint for 1 CRV received
+                // It is sent over each time the fee debt is updated on the L1 to try and keep
+                // the L2 rate as close as possible to the L1 rate
+                mintRate = (cvxAmount * 1e18) / crvAmount;
+
+                // Continue with LZ flow with crvAmount removed from payload
+                _payload = abi.encode(PT_SEND, abi.encodePacked(address(0)), abi.encodePacked(toAddress), cvxAmount);
+                super._nonblockingLzReceive(_srcChainId, _srcAddress, _nonce, _payload);
+            }
         } else {
             super._nonblockingLzReceive(_srcChainId, _srcAddress, _nonce, _payload);
         }
