f66f6221a7fb8ed6cca4b737f4e6e954a3163b4f
Fry <97352567+phijfry@users.noreply.github.com>
2023-04-14 16:40:33 +0100

    [Feat] Vote 13 April 2023 (#211)
    * feat: vote 13 apr 2023
    
    * feat: missing gauges
    
    * feat: proposal json

--- tasks/snapshot/constants.ts
+++ tasks/snapshot/constants.ts
@@ -42,18 +42,10 @@ export const months = [
 export const removedGauges = [
     "0xDc2Df969EE5E66236B950F5c4c5f8aBe62035df2", // sdBAL
     "0xcF5938cA6d9F19C73010c7493e19c02AcFA8d24D", // tetuBAL
-    // Removed as a part of https://forum.balancer.fi/t/bip-189-great-migration-wave-2/4408
-    "0xb34d43Ada4105Ff71e89b8B22a8B9562E78f01E3",
-    "0x285cBA395e3Acb82A42758638fA85da9936016a4",
-    "0x3d5F0520267FE92FFf52B847FAC3204554552f99",
-    "0xD9Ea099D62526e670aef2BD680599FC48c409f3c",
-    "0xC9Cd2B2D8744eB1E5F224612Bd7DBe7BB7d99b5A",
-    // Removed in relation to Euler hack
-    "0xD65979f15cfB52E9a6a4f8DEA34c69c1568EA4A8", // ComposableStable bb-e-USDC/bb-e-FRAX
-    "0xf53f2fEE2A34f7f8d1BFe1B774A95Cc79C121B34", // ComposableStable bb-e-USDT/bb-e-USDC/bb-e-DAI
-    "0x623F3Dbc761B46F64aE7951700Dd7724cB7d6075", // 50/50 bb-euler-USD-BPT/wstETH
-    "0x38727B907046818135c9a865D5C40BE6cd1c0514", // 50/50 TEMPLE/bb-euler-USD-BPT
-    "0xE96924D293b9e2961f9763cA058E389D27341D3d", // 50/50 bb-euler-USD-BPT/rETH
-    "0x5FbEAa96c9D8d0e839780433aA0B3B4d35b049d8", // ComposableStable bb-euler-USD-BPT/DOLA
-    "0xA9A63971c55c132aF0e6B39a081e604F07f4e234", // ComposableStable bb-a-TUSD/bb-euler-USD-BPT
+    // Removed but not killed migration
+    "0xa6325e799d266632D347e41265a69aF111b05403",
+    "0x9703C0144e8b68280b97d9e30aC6f979Dd6A38d7",
+    "0x34f33CDaED8ba0E1CEECE80e5f4a73bcf234cfac",
+    "0x2C967D6611C60274db45E0BB34c64fb5F504eDE7",
+    "0xf7C3B4e1EdcB00f0230BFe03D937e26A5e654fD4",
 ].map(x => x.toLowerCase());
