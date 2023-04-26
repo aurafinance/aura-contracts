06a569eaa65b788f34cd929526d8aecc34d63229
phijfry <phijfry@protonmail.com>
2023-04-26 21:36:28 +0100

    feat: added spend allowance to OFT

--- contracts/layerzero/token/oft/OFT.sol
+++ contracts/layerzero/token/oft/OFT.sol
@@ -30,6 +30,20 @@ contract OFT is OFTCore, ERC20, IOFT {
         return totalSupply();
     }
 
+    function _spendAllowance(
+        address owner,
+        address spender,
+        uint256 amount
+    ) internal virtual {
+        uint256 currentAllowance = allowance(owner, spender);
+        if (currentAllowance != type(uint256).max) {
+            require(currentAllowance >= amount, "ERC20: insufficient allowance");
+            unchecked {
+                _approve(owner, spender, currentAllowance - amount);
+            }
+        }
+    }
+
     function _debitFrom(
         address _from,
         uint16,
@@ -37,9 +51,7 @@ contract OFT is OFTCore, ERC20, IOFT {
         uint256 _amount
     ) internal virtual override returns (uint256) {
         address spender = _msgSender();
-        // TODO:
-        // if (_from != spender) _spendAllowance(_from, spender, _amount);
-        if (_from != spender) revert();
+        if (_from != spender) _spendAllowance(_from, spender, _amount);
         _burn(_from, _amount);
         return _amount;
     }
