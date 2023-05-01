f22f9383bc1b12b7d152bde39d320c0ed327e08d
0xahtle7 <100377534+0xahtle7@users.noreply.github.com>
2023-05-01 23:32:43 +0200

    Issue/sidechain sol improvements (#226)
    * test(sidechain): updates test BaseRewardPool4626 to allow transfers and transferFrom
    
    * refactor(sidechain): renames IGnosisBridge as IERC677 to avoid confusions

--- contracts/sidechain/bridge/GnosisBridgeSender.sol
+++ contracts/sidechain/bridge/GnosisBridgeSender.sol
@@ -2,9 +2,8 @@
 pragma solidity 0.8.11;
 
 import { Ownable } from "@openzeppelin/contracts-0.8/access/Ownable.sol";
-import { ReentrancyGuard } from "@openzeppelin/contracts-0.8/security/ReentrancyGuard.sol";
 import { BridgeDelegateSender } from "./BridgeDelegateSender.sol";
-import { IGnosisBridge } from "../interfaces/IGnosisBridge.sol";
+import { IERC677 } from "../../interfaces/IERC677.sol";
 
 contract GnosisBridgeSender is BridgeDelegateSender {
     /* -------------------------------------------------------------------
@@ -29,8 +28,8 @@ contract GnosisBridgeSender is BridgeDelegateSender {
     /// _to not in use. Currently forwards to the 'l1Delegate'
     function send(uint256 _amount) external override onlyOwner {
         require(l1Receiver != address(0), "L1ReceiverNotSet");
-        bytes memory _data = abi.encodePacked(address(l1Receiver));
-        IGnosisBridge(crv).transferAndCall(bridge, _amount, _data);
+        bytes memory data = abi.encodePacked(address(l1Receiver));
+        IERC677(crv).transferAndCall(bridge, _amount, data);
         emit Send(l1Receiver, _amount);
     }
 }
