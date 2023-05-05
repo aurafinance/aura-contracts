37e67d67b1f5eb664ff065f323c84312a0dc7134
0xahtle7 <100377534+0xahtle7@users.noreply.github.com>
2023-05-05 17:10:23 +0100

    fix(sidechain): fixes slither issues (#228)

--- contracts/sidechain/bridge/BridgeDelegateSender.sol
+++ contracts/sidechain/bridge/BridgeDelegateSender.sol
@@ -17,13 +17,17 @@ abstract contract BridgeDelegateSender is Ownable {
     address public l1Receiver;
 
     address public l2Coordinator;
-
+    /* -------------------------------------------------------------------
+       Events 
+    ------------------------------------------------------------------- */
     /**
      * @dev Emitted when tokens are sent to a recipient.
      * @param to The address of the recipient.
      * @param amount The amount of tokens sent.
      */
     event Send(address to, uint256 amount);
+    event L1ReceiverUpated(address l1Receiver);
+    event L2CoordinatorUpated(address l2Coordinator);
 
     /* -------------------------------------------------------------------
        Setter Functions
@@ -32,11 +36,13 @@ abstract contract BridgeDelegateSender is Ownable {
     function setL1Receiver(address _l1Receiver) external onlyOwner {
         require(_l1Receiver != address(0), "!0");
         l1Receiver = _l1Receiver;
+        emit L1ReceiverUpated(_l1Receiver);
     }
 
     function setL2Coordinator(address _l2Coordinator) external onlyOwner {
         require(_l2Coordinator != address(0), "!0");
         l2Coordinator = _l2Coordinator;
+        emit L2CoordinatorUpated(_l2Coordinator);
     }
 
     function send(uint256 _amount) external virtual;
