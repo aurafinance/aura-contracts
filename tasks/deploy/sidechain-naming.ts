b322b8567d9c8bd734a185416204d44aa320f9fa
phijfry <phijfry@protonmail.com>
2023-05-07 00:06:27 +0100

    fix: config cleanup

---
+++ tasks/deploy/sidechain-naming.ts
@@ -0,0 +1,9 @@
+import { SidechainNaming } from "../../types/sidechain-types";
+
+export const sidechainNaming: SidechainNaming = {
+    auraOftName: "Aura",
+    auraOftSymbol: "AURA",
+    auraBalOftName: "Aura BAL",
+    auraBalOftSymbol: "auraBAL",
+    tokenFactoryNamePostfix: " Aura Deposit",
+};
