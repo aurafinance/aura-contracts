e928efd68558a2237bc48adc1480f6df5b41a385
phijfry <phijfry@protonmail.com>
2023-06-30 15:32:22 +0100

    feat: cleanup

--- test-fork/sidechain/operations/FeeReduction.spec.ts
+++ test-fork/sidechain/operations/FeeReduction.spec.ts
@@ -1,129 +1,105 @@
-import { ethers, network } from "hardhat";
 import { expect } from "chai";
-import { config as mainnetConfig } from "../../../tasks/deploy/mainnet-config";
-import { impersonateAccount, simpleToExactAmount, ZERO_ADDRESS } from "../../../test-utils";
+import { BigNumber } from "ethers";
+import { ethers, network } from "hardhat";
+import chalk from "chalk";
+
 import { Account } from "../../../types";
+import { config as mainnetConfig } from "../../../tasks/deploy/mainnet-config";
+import { CanonicalPhase1Deployed, CanonicalPhase2Deployed } from "scripts/deploySidechain";
+import { impersonateAccount, simpleToExactAmount, ZERO_ADDRESS } from "../../../test-utils";
+import { formatEther } from "ethers/lib/utils";
 
 describe("FeeReduction", () => {
     const ethBlockNumber: number = 17591447;
 
     let deployer: Account;
     let dao: Account;
-    let multiplier;
     const sidechainFees = 2500;
     const mainnetFees = 2250;
-    let originalAura;
+    let originalAura: BigNumber;
+    let canonical: CanonicalPhase1Deployed & CanonicalPhase2Deployed;
+
+    beforeEach(async () => {
+        await network.provider.request({
+            method: "hardhat_reset",
+            params: [
+                {
+                    forking: {
+                        jsonRpcUrl: process.env.NODE_URL,
+                        blockNumber: ethBlockNumber,
+                    },
+                },
+            ],
+        });
+
+        const accounts = await ethers.getSigners();
+        deployer = await impersonateAccount(await accounts[0].getAddress());
+        dao = await impersonateAccount(mainnetConfig.multisigs.daoMultisig);
+
+        canonical = mainnetConfig.getSidechain(deployer.signer);
+        await canonical.l1Coordinator.connect(dao.signer).setDistributor(deployer.address, true);
+    });
+
+    const distributeAura = async () => {
+        const tx = await canonical.l1Coordinator
+            .connect(deployer.signer)
+            .distributeAura(110, ZERO_ADDRESS, ZERO_ADDRESS, "0x", { value: simpleToExactAmount("0.1") });
+        const logs = (await tx.wait()).logs;
+
+        let distributedAura: BigNumber;
+        let fees: BigNumber;
+
+        for (let i = 0; i < logs.length; i++) {
+            const log = logs[i];
+            if (log.address == canonical.l1Coordinator.address) {
+                const parsed = canonical.l1Coordinator.interface.parseLog(log);
+
+                if (parsed.name == "AuraDistributed") {
+                    fees = BigNumber.from(parsed.args.amount);
+                }
+            }
+            if (log.address == canonical.auraProxyOFT.address) {
+                const parsed = canonical.auraProxyOFT.interface.parseLog(log);
+
+                if (parsed.name == "SendToChain") {
+                    distributedAura = BigNumber.from(parsed.args._amount);
+                }
+            }
+        }
+
+        return { distributedAura, fees };
+    };
 
     describe("Adjust Sidechain Mint Rate", () => {
         it("Should be able to see current mint rate based on a pending distribute", async () => {
-            await network.provider.request({
-                method: "hardhat_reset",
-                params: [
-                    {
-                        forking: {
-                            jsonRpcUrl: process.env.ETHEREUM_NODE_URL,
-                            blockNumber: ethBlockNumber,
-                        },
-                    },
-                ],
-            });
-
-            const accounts = await ethers.getSigners();
-            deployer = await impersonateAccount(await accounts[0].getAddress());
-            dao = await impersonateAccount(mainnetConfig.multisigs.daoMultisig);
-
-            const sidechain = await mainnetConfig.getSidechain(deployer.signer);
-
-            await sidechain.l1Coordinator.connect(dao.signer).setDistributor(deployer.address, true);
-            const tx = await sidechain.l1Coordinator
-                .connect(deployer.signer)
-                .distributeAura(110, ZERO_ADDRESS, ZERO_ADDRESS, "0x", { value: simpleToExactAmount("0.1") });
-            const logs = (await tx.wait()).logs;
-
-            let distributedAura;
-            let fees;
-
-            for (let i = 0; i < logs.length; i++) {
-                const log = logs[i];
-                if (log.address == sidechain.l1Coordinator.address) {
-                    const parsed = sidechain.l1Coordinator.interface.parseLog(log);
-
-                    if (parsed.name == "AuraDistributed") {
-                        fees = parsed.args.amount;
-                    }
-                }
-                if (log.address == sidechain.auraProxyOFT.address) {
-                    const parsed = sidechain.auraProxyOFT.interface.parseLog(log);
-
-                    if (parsed.name == "SendToChain") {
-                        distributedAura = parsed.args._amount;
-                    }
-                }
-            }
+            const { distributedAura, fees } = await distributeAura();
 
             originalAura = distributedAura;
 
-            console.log("Multiplier: " + 10000);
-            console.log("Aura Sent: " + Number(distributedAura) / 1e18);
-            console.log("Bal Fees: " + Number(fees) / 1e18);
+            // prettier-ignore
+            {
+              console.log(`Multiplier:            ${1000}`);
+              console.log(`New Aura Sent:         ${formatEther(distributedAura)}`);
+              console.log(`Bal Fees:              ${formatEther(fees)}`);
+            }
         });
 
         it("Should be able to adjust the mint rate to account for fees", async () => {
-            await network.provider.request({
-                method: "hardhat_reset",
-                params: [
-                    {
-                        forking: {
-                            jsonRpcUrl: process.env.ETHEREUM_NODE_URL,
-                            blockNumber: ethBlockNumber,
-                        },
-                    },
-                ],
-            });
-
-            const accounts = await ethers.getSigners();
-            deployer = await impersonateAccount(await accounts[0].getAddress());
-            dao = await impersonateAccount(mainnetConfig.multisigs.daoMultisig);
-
-            const sidechain = await mainnetConfig.getSidechain(deployer.signer);
-
             const adjustmentNeeded = 1.0 - (sidechainFees / mainnetFees / (sidechainFees - mainnetFees)) * 10;
-            multiplier = Math.floor(adjustmentNeeded * 10000);
+            const multiplier = Math.floor(adjustmentNeeded * 10000);
 
-            await sidechain.l1Coordinator.connect(dao.signer).setRewardMultiplier(multiplier);
-            expect(await sidechain.l1Coordinator.rewardMultiplier()).eq(multiplier);
+            await canonical.l1Coordinator.connect(dao.signer).setRewardMultiplier(multiplier);
+            expect(await canonical.l1Coordinator.rewardMultiplier()).eq(multiplier);
 
-            await sidechain.l1Coordinator.connect(dao.signer).setDistributor(deployer.address, true);
-            const tx = await sidechain.l1Coordinator
-                .connect(deployer.signer)
-                .distributeAura(110, ZERO_ADDRESS, ZERO_ADDRESS, "0x", { value: simpleToExactAmount("0.1") });
-            const logs = (await tx.wait()).logs;
+            const { distributedAura, fees } = await distributeAura();
 
-            let distributedAura;
-            let fees;
-
-            for (let i = 0; i < logs.length; i++) {
-                const log = logs[i];
-                if (log.address == sidechain.l1Coordinator.address) {
-                    const parsed = sidechain.l1Coordinator.interface.parseLog(log);
-
-                    if (parsed.name == "AuraDistributed") {
-                        fees = parsed.args.amount;
-                    }
-                }
-                if (log.address == sidechain.auraProxyOFT.address) {
-                    const parsed = sidechain.auraProxyOFT.interface.parseLog(log);
-
-                    if (parsed.name == "SendToChain") {
-                        distributedAura = parsed.args._amount;
-                    }
-                }
+            // prettier-ignore
+            {
+              console.log(`New Multiplier:        ${multiplier}`);
+              console.log(`New Aura Sent:         ${formatEther(distributedAura)}`);
+              console.log(`Change In Aura Sent:   ${chalk.red("-")}${chalk.red(formatEther(originalAura.sub(distributedAura)))}`);
+              console.log(`Bal Fees:              ${formatEther(fees)}`);
             }
-
-            console.log("New Multiplier: " + multiplier);
-            console.log("New Aura Sent: " + Number(distributedAura) / 1e18);
-            console.log("Change In Aura Sent: " + -(Number(originalAura) - Number(distributedAura)) / 1e18);
-            console.log("Bal Fees: " + Number(fees) / 1e18);
         });
     });
 });
