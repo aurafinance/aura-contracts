32f75496b1b9719ab749bdc13e091faf833a1d0c
phijfry <phijfry@protonmail.com>
2023-07-18 22:12:24 +0100

    fix: linting

--- tasks/sidechain/polygon-bridging.ts
+++ tasks/sidechain/polygon-bridging.ts
@@ -3,8 +3,8 @@ import { getSigner } from "../utils";
 
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment, TaskArguments } from "hardhat/types";
-import { config } from "../deploy/polygon-config";
 import { ethers } from "ethers";
+import _axios from "axios";
 
 task("sidechain:polygon:bridge")
     .addParam("txhash", "L2 TXN Hash of the bridge withdrawal")
@@ -15,10 +15,9 @@ task("sidechain:polygon:bridge")
         const mainnetBridge = "0xA0c68C638235ee32657e8f720a23ceC1bFc77C77";
         const baseURL = "https://proof-generator.polygon.technology/api/v1/matic/exit-payload/";
         const event = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
-        const eventSignature = "?eventSignature="
+        const eventSignature = "?eventSignature=";
 
-
-        const axios = require("axios").create({
+        const axios = _axios.create({
             baseURL,
         });
 
@@ -26,11 +25,10 @@ task("sidechain:polygon:bridge")
             params: { limit: 100, offset: 0 },
         });
 
-
-        if(withdrawalData.data.message == "Payload generation success"){
+        if (withdrawalData.data.message == "Payload generation success") {
             const proof = withdrawalData.data.result;
             const abi = ["function exit(bytes inputData)"];
-    
+
             const bridgeContract = new ethers.Contract(mainnetBridge, abi);
             await bridgeContract.connect(deployer).exit(proof);
         }
