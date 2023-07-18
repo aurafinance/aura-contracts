90e9839fdad3ac8d824441b88ad89ca5c3b45057
Hephyrius <20884880+Hephyrius@users.noreply.github.com>
2023-07-18 16:26:24 +0200

    feat: working exit

--- tasks/sidechain/polygon-bridging.ts
+++ tasks/sidechain/polygon-bridging.ts
@@ -3,10 +3,8 @@ import { getSigner } from "../utils";
 
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment, TaskArguments } from "hardhat/types";
-// import { config } from "../deploy/polygon-config";
+import { config } from "../deploy/polygon-config";
 import { ethers } from "ethers";
-
-import pos from "@maticnetwork/maticjs";
 
 task("sidechain:polygon:bridge")
     .addParam("txhash", "L2 TXN Hash of the bridge withdrawal")
@@ -14,21 +12,26 @@ task("sidechain:polygon:bridge")
         const deployer = await getSigner(hre);
         const withdrawTxHash = tskArgs.txhash;
 
-        const polygonProvider = new ethers.providers.JsonRpcProvider(process.env.OPTIMISM_NODE_URL);
-
-        const options = {
-            network: "ethereum",
-            version: "v1",
-            maticProvider: polygonProvider,
-            parentProvider: deployer,
-        };
-
-        // console.log(Matic);
-
-        const maticClient = new pos.POSClient();
-        await maticClient.init(options);
+        const mainnetBridge = "0xA0c68C638235ee32657e8f720a23ceC1bFc77C77";
+        const baseURL = "https://proof-generator.polygon.technology/api/v1/matic/exit-payload/";
         const event = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
-        const tx = await maticClient.exitUtil.buildPayloadForExit(withdrawTxHash, event, false);
+        const eventSignature = "?eventSignature="
 
-        console.log(tx);
+
+        const axios = require("axios").create({
+            baseURL,
+        });
+
+        const withdrawalData = await axios.get(withdrawTxHash + eventSignature + event, {
+            params: { limit: 100, offset: 0 },
+        });
+
+
+        if(withdrawalData.data.message == "Payload generation success"){
+            const proof = withdrawalData.data.result;
+            const abi = ["function exit(bytes inputData)"];
+    
+            const bridgeContract = new ethers.Contract(mainnetBridge, abi);
+            await bridgeContract.connect(deployer).exit(proof);
+        }
     });
