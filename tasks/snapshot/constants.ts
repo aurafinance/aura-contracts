985a5ce564acbfdd4f31cc98d8a35cdeb58c54f4
phijfry <phijfry@protonmail.com>
2023-09-14 07:44:48 +0100

    feat: gauge choices

--- tasks/snapshot/constants.ts
+++ tasks/snapshot/constants.ts
@@ -62,6 +62,7 @@ export const months = [
 export const removedGauges = [
     "0xDc2Df969EE5E66236B950F5c4c5f8aBe62035df2", // sdBAL
     "0xcF5938cA6d9F19C73010c7493e19c02AcFA8d24D", // tetuBAL
+    "0xd1177e2157a7fd6a0484b79f8e50e8a9305f8063", // tetuBAL
     // Removed but not killed yet 17th Jul 2023
     // https://forum.balancer.fi/t/bip-356-kill-affected-csp-and-weighted-pool-gauges/4971
     "0xA2a9Ebd6f4dEA4802083F2C8D08066A4e695e64B",
